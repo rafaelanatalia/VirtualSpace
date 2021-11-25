61340fa339b3034fa4a3c2a4b54cd8e32ae6abaf
Rafaela Natalia <rafaela.natalia1998@hotmail.com>
2021-11-24 23:04:45 -0300

    terminando a parte index

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+/*menu mobile*/
+
+const btnmobile =document.getElementById(btn-mobile);
+
+function toggleMenu(){
+    const ul =document.getElementById(ul);
+    ul.classList.toggle(active)
+}
+
+btnmobile.addEventListener('click',toglleMenu)
