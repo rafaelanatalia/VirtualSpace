538f5508389e57718eb61c757a12f6e9ad6a83ce
Rafaela Natalia <rafaela.natalia1998@hotmail.com>
2021-11-25 11:46:34 -0300

    arrumando as pastas

---
+++ public/assets/js/main.js
@@ -0,0 +1,12 @@
+/*menu mobile*/
+
+const btnmobile =document.querySelector("#btnmobile");
+
+function toggleMenu(){
+    const nav =document.querySelector("#nav-menu");
+    nav.classList.toggle(active)
+}
+
+btnmobile.addEventListener('click',toglleMenu)
+
+
