045baf7920da9106c25cec23a89fd1c8233026a6
Rafaela <rafaela.natalia1998@hotmail.com>
2022-02-22 10:50:17 -0300

    arrumando as pastas

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // impostando o express
 const express = require("express");
 const AdmController = require("./controller/AdmController");
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 
 //importando os roteadores
@@ -11,12 +15,16 @@ const AdmRouter = require('./routers/AdmRouter');
 const server=express();
 
 //configurando o view engine para ejs
-server.set("view engine","ejs");
-//necessario apenas caso precise renomear a pasta ou rota
-server.set('views','./views');
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'ejs');
 
+server.use(logger('dev'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cookieParser());
 //config a pasta public
-server.use(express.static(__dirname+ '/public'));
+server.use(express.static(path.join(__dirname, 'public')));
+
 
 //JSON
 server.use(express.urlencoded({extended: false}));
@@ -26,6 +34,23 @@ server.use(express.json());
 server.use('/', VirtualRouter);
 server.use('/adm', AdmRouter);
 
+// catch 404 and forward to error handler
+server.use(function(req, res, next) {
+
+    next(createError(404));
+    
+  });
+  
+  // error handler
+  server.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 //levantando o servidor
 server.listen(5000,()=>{
