10147724ddbb235ebcfb28b46fda4558619dc7c0
Gabriel Patara <gabriel.patara@gmail.com>
2022-02-22 11:03:00 -0300

    pasta pra testar bd

---
+++ server/models/cliente.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class cliente extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  cliente.init({
+    nome: DataTypes.STRING,
+    email: DataTypes.STRING,
+    senha: DataTypes.STRING,
+    endereco: DataTypes.STRING,
+    telefone: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'cliente',
+  });
+  return cliente;
+};
